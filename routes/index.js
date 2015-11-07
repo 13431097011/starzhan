d6e463522723377154173278994c97980881ccca
unknown <StarZhan@boyaa.com>
2015-11-07 15:25:21 +0800

    zxx

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+//var express = require('express');
+//var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+module.exports = function(app){
+	app.get('/', function(req, res) {
+		res.render('index', { active: {index:'active'} });
+	});
+	app.get('/content',function(req,res){
+		res.render('content', { active: {content:'active'} });
+	});
+	app.get('/post',function(req,res){
+		res.render('post', { active: {post:'active'} });
+	});
+};
