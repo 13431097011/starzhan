6027342f5e7288144ceaafdb69999008b521444e
unknown <StarZhan@boyaa.com>
2015-11-27 12:13:34 +0800

    zhanxunxin asd

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,49 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 module.exports = function(app){
-	
 	app.get('/', function(req, res) {
-		res.render('index', {
-			active: {index:'active'},
-			user:req.session.user,
-			success:req.flash('success').toString(),
-			error:req.flash('error').toString()
-
-		 });
+		Post.get(null,function(err,posts){
+			if(err){
+				posts = [];
+			}
+			res.render('index', {
+				active: {index:'active'},
+				user:req.session.user,
+				posts:posts,
+				success:req.flash('success').toString(),
+				error:req.flash('error').toString()
+			});
+		});
 	});
 	app.get('/content',function(req,res){
 		res.render('content', { active: {content:'active'} });
 	});
+	app.get('/post',checkLogin);
 	app.get('/post',function(req,res){
-		res.render('post', { active: {post:'active'} });
-	});
-	app.get('/login',function(req,res){
-		res.render('login', { 
-			active: {login:'active'},
+		res.render('post', {
+			active: {post:'active'},
 			user:req.session.user,
 			success:req.flash('success').toString(),
 			error:req.flash('error').toString()
-		 });
+		});
 	});
+	app.post('/post',checkLogin);
+	app.post('/post',function(req,res){
+		var currentUser = req.session.user;
+		var post = new Post(currentUser.name,req.body.title,req.body.post);
+		post.save(function(err){
+			if(err){
+				req.flash('error',err);
+				return res.redirect('/');
+			}
+			req.flash('success','发布成功!');
+			res.redirect('/');
+		});
+	});
+
+
+	app.get('/reg',checkNotLogin);
 	app.get('/reg',function(req,res){
 		res.render('reg', { 
 			active: {reg:'active'},
@@ -33,6 +52,7 @@ module.exports = function(app){
 			error:req.flash('error').toString()
 		 });
 	});
+	app.post('/reg',checkNotLogin);
 	app.post('/reg',function(req,res){
 		var name = req.body.name;
 		var pwd = req.body.pwd;
@@ -67,6 +87,16 @@ module.exports = function(app){
 			});
 		});
 	});
+	app.get('/login',checkNotLogin);
+	app.get('/login',function(req,res){
+		res.render('login', { 
+			active: {login:'active'},
+			user:req.session.user,
+			success:req.flash('success').toString(),
+			error:req.flash('error').toString()
+		 });
+	});
+	app.post('/login',checkNotLogin);
 	app.post('/login',function(req,res){
 		var md5 = crypto.createHash('md5');
 		var pwd = md5.update(req.body.pwd).digest('hex');
@@ -85,10 +115,26 @@ module.exports = function(app){
 			return res.redirect('/');
 		});
 	});
+	app.get('/logout',checkLogin)
 	app.get('/logout',function(req,res){
 		req.session.user = null;
 		req.flash('success','退出成功！');
 		return res.redirect('/');
 	});
+
+	function checkLogin(req,res,next){
+		if(!req.session.user){
+			req.flash('error','未登录！');
+			res.redirect('/login');
+		}
+		next();
+	}
+	function checkNotLogin(req,res,next){
+		if(req.session.user){
+			req.flash('error','已登录!');
+			res.redirect('back');
+		}
+		next();
+	}
 
 };
